86b5fd8265fc11c48f906626385953ba585fe7f3
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-01-29 12:22:17 +0000

    Completed Red-9

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,26 +1,56 @@
 import Piece from './piece';
 import Square from '../square';
+import Player from '../player';
+
 
 export default class Rook extends Piece {
     constructor(player) {
         super(player);
     }
+    // 
+    addToMovesIfAvailable(board,square) {
+        if(!board.isSquareOccupied(square)) 
+            this.availableMoves.push(square);
+    }
 
     getAvailableMoves(board) {
 
         let location = board.findPiece(this);
-        // Horizontal
-        let availableMoves = [];
-        for(let position = 0 ; position <=7 ; position++) {
+        // Horizontally to the left
+        for(let position = location.col ; position >= 0 ; position--) {
             if(position !== location.col)
-                availableMoves.push(Square.at(location.row, position));
-        }
-        // Vertical
-        for(let position = 0 ; position <=7 ; position++) {
-            if(position !== location.row)
-                availableMoves.push(Square.at(position, location.col));
+                if (board.isSquareOccupied(Square.at(location.row, position))) 
+                    break;//check if occupied
+                else
+                    this.addToMovesIfAvailable(board, Square.at(location.row, position));
         }
 
-        return availableMoves;
+        // Horizontally to the right
+        for(let position = location.col ; position <= 7 ; position++) {
+            if(position !== location.col)
+                if (board.isSquareOccupied(Square.at(location.row, position))) 
+                    break;//check if occupied
+                else
+                    this.addToMovesIfAvailable(board, Square.at(location.row, position));
+        }
+        
+        // Vertically to the top
+        for(let position = location.row ; position <= 7 ; position++) {
+            if(position !== location.row)
+                if (board.isSquareOccupied(Square.at(position, location.col))) 
+                    break;//check if occupied
+                else
+                    this.addToMovesIfAvailable(board, Square.at(position, location.col));
+        }
+
+        // Vertically to the bottom
+        for(let position = location.row ; position >= 0 ; position--) {
+            if(position !== location.row)
+                if (board.isSquareOccupied(Square.at(position, location.col))) 
+                    break;//check if occupied
+                else
+                    this.addToMovesIfAvailable(board, Square.at(position, location.col));
+        }
+        return this.availableMoves;
     }
 }
