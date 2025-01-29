c70ea0503554ed28616c55f9b7c40d9b60a818b8
Kalaivani Natarajan <kalaivani.natarajan@softwire.com>
2025-01-29 11:45:49 +0000

    added piece changes

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,12 +1,14 @@
 export default class Piece {
     constructor(player) {
         this.player = player;
+        this.availableMoves = [];
     }
 
     getAvailableMoves(board) {
         throw new Error('This method must be implemented, and return a list of available moves');
     }
 
+    
     moveTo(board, newSquare) {
         const currentSquare = board.findPiece(this);
         board.movePiece(currentSquare, newSquare);
